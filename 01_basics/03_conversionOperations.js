4845f3c610065c06db04e37ebb6013cf4bbb2628
D M TARUN RAJ <141480796+dmtarunraj@users.noreply.github.com>
2024-06-03 12:21:21 +0000

    operations on datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -13,9 +13,32 @@
 
 let isLoggedIn = " dcndacnj"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //1 => true
 // "" => false
 // "kdfvnd" => true 
+
+
+// *************************** Operations ************************
+
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+
+let str1="Hello"
+let str2="hi"
+let str3= str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+2+3);
+// console.log(3+2+"4");
+
+// console.log(3+4+6*4);
+
+//console.log(-true);
+
