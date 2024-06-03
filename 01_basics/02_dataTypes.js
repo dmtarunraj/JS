6d6e4b908f5886da30cc65022a59d6a27fde2cea
D M TARUN RAJ <141480796+dmtarunraj@users.noreply.github.com>
2024-06-03 11:40:17 +0000

    data_types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all js code as newer version
+
+// alert("3+3")  // we are nodejs, not browser
+
+console.log(3+3)
+
+// cod redeability should be high
+
+console.log("Tarun")
+
+let name = "Tarun"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value 
+// undefined => value not assigned
+// symbol => unique 
+
+
+
+// object 
+
+console.log(typeof null); // object
+
+console.log(typeof undefined) // undefined 
