0852e7456353d026f17e1da828781e905d4fea9f
D M TARUN RAJ <141480796+dmtarunraj@users.noreply.github.com>
2024-06-03 12:01:22 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+// let score = "sjfvjsj"
+// console.log(typeof (score));
+
+
+// let valuInNumber = Number(score)
+// console.log(typeof (valuInNumber));
+// console.log(valuInNumber);
+
+
+// "33" =>33
+// "33kmkd" => NaN
+// true => 1
+
+let isLoggedIn = " dcndacnj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true
+// "" => false
+// "kdfvnd" => true 
