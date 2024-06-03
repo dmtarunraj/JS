9e43430dd275bfa1c2cbd4b7a5efeb8e0ad19509
D M TARUN RAJ <141480796+dmtarunraj@users.noreply.github.com>
2024-06-03 12:53:10 +0000

    data tpes summary

---
+++ 01_basics/dataTypes_Summary.js
@@ -0,0 +1,21 @@
+// primitive 
+
+// 7 types : string, Number , Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 200
+
+const isLoggedIn = false
+
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const aniotherId = ("123")
+
+console.log(id === aniotherId)
+
+
+// reference (Non primitive)
+
+// Array, Objects, Functions
