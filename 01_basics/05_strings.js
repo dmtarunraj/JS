a6ce271a64dab1aeaa057ba6f5c0a111bb74d6da
D M TARUN RAJ <141480796+dmtarunraj@users.noreply.github.com>
2024-09-11 16:12:34 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "Tarun"
+
+const repoCount = 10
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Tarun") 
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+  
+const sliceString = gameName.slice(0,2)
+console.log(sliceString);
+
+
